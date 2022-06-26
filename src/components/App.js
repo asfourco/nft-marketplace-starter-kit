ff269cbb985ff9c2ea214b6b448fe9a11054d69d
Fadi Asfour <fadi.asfour@gmail.com>
2022-06-26 14:53:57 -0300

    section 24

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import Web3 from 'web3';
 import detectEthereumProvider from '@metamask/detect-provider';
 import KryptoBird from '../abis/KryptoBird.json';
 
+import {MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardImage, MDBBtn} from 'mdb-react-ui-kit';
+import './App.css';
+
 
 class App extends Component {
 
@@ -54,7 +57,7 @@ class App extends Component {
     mint = (kryptoBird) => {
         this.state.contract.methods.mint(kryptoBird).send({ from: this.state.account }).once('receipt', (receipt) => {
             this.setState({
-                kryptoBirdz: [...this.state.kryptoBirdz, receipt]
+                kryptoBirdz: [...this.state.kryptoBirdz, KryptoBird]
             })
         })
     }
@@ -71,7 +74,8 @@ class App extends Component {
 
     render() {
         return (
-            <div>
+            <div className='container-filled'>
+                {console.log(this.state.kryptoBirdz)}
                 <nav className='navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'>
                     <div className='navbar-brand col-sm-3 col-md-3 mr-0' style={{ color: 'white' }}>
                         Krypto Birdz NFTs (Non Fungible Tokens)
@@ -89,7 +93,8 @@ class App extends Component {
                     <div className='row'>
                         <main role='main' className='col-lg-12 d-flex text-center'>
                             <div className='content mr-auto ml-auto' sytle={{ opacity: '0.8' }}>
-                                <h1> KryptoBridz - NFT Marketplace</h1>
+                            <h1 style={{color:'black'}}>
+                                    KryptoBirdz - NFT Marketplace</h1>
 
                                 <form onSubmit={(event) => {
                                     event.preventDefault();
@@ -107,10 +112,28 @@ class App extends Component {
                                         className='btn btn-primary btn-black'
                                         value='MINT'
                                     />
-
                                 </form>
                             </div>
                         </main>
+                    </div>
+                    <hr></hr>
+                    <div className='row textCenter'>
+                        {this.state.kryptoBirdz.map((kryptobird, key) => {
+                            return (
+                                <div>
+                                    <div>
+                                        <MDBCard className='token img' style={{maxWidth: '22rem'}}>
+                                            <MDBCardImage src={kryptobird} position='top' height='250rem' style={{marginRight:'4px'}} />
+                                            <MDBCardBody>
+                                                <MDBCardTitle> KryptoBridz </MDBCardTitle>
+                                                <MDBCardText>The KryptoBirdz are 20 uniquely generated KBirdz from the cyberpunk cloud galaxy Mystopia! There is only one of each bird and each bird can be owned by a single person on the Ethereum blockchain. </MDBCardText>
+                                                <MDBBtn href={kryptobird}>Download</MDBBtn>
+                                            </MDBCardBody>
+                                        </MDBCard>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </div>
